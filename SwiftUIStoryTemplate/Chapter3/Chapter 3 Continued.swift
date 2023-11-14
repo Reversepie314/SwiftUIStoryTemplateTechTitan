7bd0b2365f411f99c2733b10bb4bccb5727f7194
Reversepie314 <ka6johnson.kj@gmail.com>
2023-11-14 09:09:44 -0500

    AddPhotos

---
+++ SwiftUIStoryTemplate/Chapter3/Chapter 3 Continued.swift
@@ -0,0 +1,20 @@
+//
+//  Chapter 3 Continued.swift
+//  TechTitanSwiftUI
+//
+//  Created by Keon Johnson on 11/14/23.
+//
+
+import SwiftUI
+
+struct Chapter_3_Continued: View {
+    var body: some View {
+        VStack {
+            Image("ForkInRoad")
+        }
+    }
+}
+
+#Preview {
+    Chapter_3_Continued()
+}
