f60866953fb9cdfa136cb120583db4c1f3affdc8
Reversepie314 <ka6johnson.kj@gmail.com>
2023-11-16 11:26:06 -0500

    We got this (Hopefully)

--- SwiftUIStoryTemplate/Chapter5/Chapter5Page50.swift
+++ SwiftUIStoryTemplate/Chapter5/Chapter5Page50.swift
@@ -9,66 +9,81 @@ import SwiftUI
 
 struct Chapter5Continued: View {
     var body: some View {
-        VStack{
-            Text("Conclusion")
-                .font(.largeTitle)
-                .foregroundColor(.green)
-                .padding()
+        ScrollView {
+            VStack{
+                
+                Text("Epilogue")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                
+                
+                Text(
+                    "Our friends have returned to town to face the evil darkness, Alex and the gang have gained the power to defeat the dark entity."
+                ).padding()
+                
+                Text(
+                    "As the battle with the Shadowlord concluded, and the darkness receded, the five friends stood amidst the now-peaceful forest, their hearts still racing from the intensity of the struggle. The weight of their heroic actions hung in the air, and a sense of accomplishment filled their hearts."
+                ) .padding()
+                
+                Text(
+                    "'A sign of relief, our heroes' journey has concluded.'"
+                )
+                .font(.custom("SnellRoundhand-Bold", size: 36)).padding()
+                Text(
+                    "Lily, the group's gifted healer, broke the silence with a gentle smile. 'We did it,' she said, her eyes filled with gratitude. 'We've not only saved Everwood, but the entire world.'"
+                ) .padding()
+                
+                Text(
+                    "Max, who possessed a deep connection with nature, nodded in agreement. 'And we did it together. The bond we've formed is unbreakable.'"
+                )  .padding()
+                
+                Text(
+                    "Mia, the skilled archer, turned to Jake, the fearless warrior, and said, 'Jake, your bravery in the face of danger was truly inspiring.'"
+                ) .padding()
+                
+                Text(
+                    "Jake grinned and clapped a hand on Alex's shoulder, the brilliant strategist of the group. 'And Alex, your plans and wisdom guided us to victory. We couldn't have done it without you.'"
+                ) .padding()
+                
+                Text("Alex humbly nodded, acknowledging their words. 'But it was your strength and courage that made the difference.'")
+                    .padding()
+                
+                Text("With a sense of unity and shared accomplishment, they gathered in a circle, each placing a hand on the shoulder of the person next to them.")
+                    .padding()
+                
+                        Text("'I believe our journey has given us more than just victory,' Mia said. 'It has given us a power we never knew we had.'")
+                    .padding()
+                     
+                
+                        Text("Max, his eyes alight with realization, added, 'The power of friendship, unity, and unwavering belief in each other.'")
+                    .padding()
+                     
+                
+                        Text("Lily smiled and looked at her friends. 'And with this newfound power, we can bring light to any darkness.'")
+                    .padding()
+                
+                        Text("And so, in that quiet forest clearing, they formed a pact. 'I'm going to share my power,' Alex said, and one by one, they all echoed the sentiment, committing to stand together in the face of any challenge.")
+                    .padding()
+                
+                        Text("A ghostly figure appeared from the horizon staring at our heroes from afar")
+                    .padding()
+                
+                        Text("The Guardians of Light saw the ghostly figure from afar and watched her descend into thin air.")
+                    .padding()
+                
+                        Text("Their epic journey had indeed transformed them, not only into heroes but also into lifelong friends, forever bound by the memory of their extraordinary quest. As they made their way back to Everwood, the town greeted them with cheers, celebration, and gratitude, and they were forever etched in the annals of history as The Guardians of Light.")
+                    .padding()
+                
+                        Text("Everwood lived on, basking in the light of hope, the enduring legacy of The Guardians of Light, and the newfound power of their unwavering friendship.")
+                    .padding()
+                
+            } .padding()
             
-            
-            Text(
-                "Our friends have returned to town to face the evil darkness, Alex and the gang have gained the power to defeat the dark entity."
-            )
-            
-            Text(
-                "As the battle with the Shadowlord concluded, and the darkness receded, the five friends stood amidst the now-peaceful forest, their hearts still racing from the intensity of the struggle. The weight of their heroic actions hung in the air, and a sense of accomplishment filled their hearts."
-            )
-            
-            Text(
-                "A sign of relief, our heroes' journey has concluded."
-            )
-            Text(
-                "Lily, the group's gifted healer, broke the silence with a gentle smile. 'We did it,' she said, her eyes filled with gratitude. 'We've not only saved Everwood, but the entire world.'"
-            )
-            
-            Text(
-                "Max, who possessed a deep connection with nature, nodded in agreement. 'And we did it together. The bond we've formed is unbreakable.'"
-            )
-            
-            Text(
-                "Mia, the skilled archer, turned to Jake, the fearless warrior, and said, 'Jake, your bravery in the face of danger was truly inspiring.'"
-            )
-            
-            Text(
-                "Jake grinned and clapped a hand on Alex's shoulder, the brilliant strategist of the group. 'And Alex, your plans and wisdom guided us to victory. We couldn't have done it without you.'"
-            )
-            //
-            //        "Alex humbly nodded, acknowledging their words. 'But it was your strength and courage that made the difference.'"
-            //
-            //        "With a sense of unity and shared accomplishment, they gathered in a circle, each placing a hand on the shoulder of the person next to them."
-            //
-            //        "'I believe our journey has given us more than just victory,' Mia said. 'It has given us a power we never knew we had.'"
-            //
-            //        "Max, his eyes alight with realization, added, 'The power of friendship, unity, and unwavering belief in each other.'"
-            //
-            //        "Lily smiled and looked at her friends. 'And with this newfound power, we can bring light to any darkness.'"
-            //
-            //        "And so, in that quiet forest clearing, they formed a pact. 'I'm going to share my power,' Alex said, and one by one, they all echoed the sentiment, committing to stand together in the face of any challenge."
-            //
-            //        "A ghostly figure appeared from the horizon staring at our heroes from afar"
-            //
-            //        "The Guardians of Light saw the ghostly figure from afar and watched her descend into thin air."
-            //
-            //        "Their epic journey had indeed transformed them, not only into heroes but also into lifelong friends, forever bound by the memory of their extraordinary quest. As they made their way back to Everwood, the town greeted them with cheers, celebration, and gratitude, and they were forever etched in the annals of history as The Guardians of Light."
-            //
-            //        "Everwood lived on, basking in the light of hope, the enduring legacy of The Guardians of Light, and the newfound power of their unwavering friendship."
             
         }
-        
-        
     }
 }
-
 #Preview {
     Chapter5Continued()
 }
